edc68db7f27c2c836e5c34c52bb96ee0a74a8ab0
riumr <atrium_grate.0j@icloud.com>
2024-02-15 16:24:47 +0900

    layout : 기본 레이아웃 파일 추가

---
+++ portfolio_react/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import CategoryGroup from "./components/CategoryGroup";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <CategoryGroup />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
