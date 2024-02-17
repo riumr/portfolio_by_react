28c5030d1f08c9942e7ff691f29e56e602669622
riumr <atrium_grate.0j@icloud.com>
2024-02-17 19:13:25 +0900

    feat : Main 컴포넌트에 Projects 컴포넌트 추가

--- portfolio_react/src/components/Main.jsx
+++ portfolio_react/src/components/Main.jsx
@@ -1,9 +1,11 @@
 import Skills from "./Skills"
+import Projects from "./Projects"
 
 export default function Main() {
     return (
         <>
             <Skills/>
+            <Projects/>
         </>
     )
 }
