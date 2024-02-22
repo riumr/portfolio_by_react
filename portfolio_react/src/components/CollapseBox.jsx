7bf4074b1512557974b89310b86b585360658d02
riumr <atrium_grate.0j@icloud.com>
2024-02-22 18:39:09 +0900

    test : 이미지 가져오기

--- portfolio_react/src/components/CollapseBox.jsx
+++ portfolio_react/src/components/CollapseBox.jsx
@@ -1,22 +1,22 @@
 export default function CollapseBox (props) {
     const boxId = props.boxId
-    const boxImg = props.boxImgSrc
+    const boxImg = props.boxImg
     const collapseStyle = {
         height:'50px',
     }
     const boxStyle = {
-        trainsition: 'height 0.5s ease',
+        transition: 'height 0.5s ease',
     }
     return(
         <>
             <div className="btn btn-outline-secondary d-flex justify-content-center align-items-center w-100 mt-3"
                 data-bs-toggle="collapse" data-bs-target={`#${boxId}`} aria-expanded="false" aria-controls={boxId} style={collapseStyle}>
-                    누르면 열림
+                    상세내용 보기
             </div>
             <div id={boxId} className="collapse" style={boxStyle}>
-            <div className="card card-body mt-3">
-                <img src={boxImg} alt={`${boxId}img`}/>
-            </div>
+                <div className="card card-body mt-3">
+                    <img src={`image/spaCollapseImg.png`} alt={`${boxId}img`}/>
+                </div>
             </div>
         </>
     )
--- portfolio_react/src/components/Projects.jsx
+++ portfolio_react/src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import Carousel from "./Carousel"
 import ContentBox from "./ContentBox"
 import CollapseBox from "./CollapseBox"
+import { spaPjtImg } from "./ImgPath"
 
 export default function Projects() {
     return (
@@ -10,28 +11,33 @@ export default function Projects() {
                     <h2 className="fs-1 fw-bold mt-5">PROJECTS</h2>
                     {/* Personal Project */}
                     <div className="pt-3">
-                        <Project projectName="Personal Project" carouselId="" contentTitle="" contentDescription=""/>
-                        <CollapseBox boxId="" boxImgSrc=""/>
+                        <Project 
+                            projectName="Personal Project" carouselId="" carouselImg={spaPjtImg}
+                            contentTitle="SPA 게시판" contentDescription="" contentLink=""
+                        />
+                        <CollapseBox boxId="spa" boxImgSrc="image/spaCollpaseImg.png"/>
                     </div>
                     {/* Team Project */}
                     <div className="pt-3">
-                        <Project projectName="Team Project" carouselId="" contentTitle="" contentDescription=""/>
+                        <Project
+                            projectName="Team Project" carouselId="" carouselImg={spaPjtImg} 
+                            contentTitle="" contentDescription="" contentLink=""
+                        />
                         <CollapseBox boxId="" boxImgSrc=""/>
                     </div>
                     {/* profile page */}
                     <div className="pt-3">
-                        <Project projectName="Profile Page" carouselId="" contentTitle="" contentDescription=""/>
-                        <CollapseBox boxId="" boxImgSrc=""/>
-                    </div>
-                    {/* crud project */}
-                    <div className="pt-3">
-                        <Project projectName="CRUD Project" carouselId="" contentTitle="" contentDescription=""/>
-                        <CollapseBox boxId="" boxImgSrc=""/>
+                        <Project
+                            projectName="Profile Page" carouselId="" carouselImg={spaPjtImg} 
+                            contentTitle="" contentDescription="" contentLink=""
+                        />
                     </div>
                     {/* Text 분석 프로젝트 */}
                     <div className="pt-3">
-                        <Project projectName="Text Analysis Project" carouselId="" contentTitle="" contentDescription=""/>
-                        <CollapseBox boxId="" boxImgSrc=""/>
+                        <Project
+                            projectName="Text Analysis Project" carouselId="" carouselImg={spaPjtImg} 
+                            contentTitle="" contentDescription="" contentLink=""
+                        />
                     </div>
                 </div>
             </div>
@@ -41,14 +47,16 @@ export default function Projects() {
 
 const Project = (props) => {
     const carouselId = props.carouselId
+    const carouselImg = props.carouselImg
     const contentTitle = props.contentTitle
     const contentDescription = props.contentDescription
+    const contentLink = props.contentLink
     return (
         <div className="pt-3">
             <h3 className="semibold">{props.projectName}</h3>
             <div className="d-flex flex-wrap py-2">
-                <Carousel carouselId={carouselId}/>
-                <ContentBox title={contentTitle} description={contentDescription}/>
+                <Carousel carouselId={carouselId} carouselImg={carouselImg}/>
+                <ContentBox title={contentTitle} description={contentDescription} link={contentLink}/>
             </div>
         </div>
     )
