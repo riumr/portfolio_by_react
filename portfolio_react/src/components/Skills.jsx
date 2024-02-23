e1d1f058790e9fd652e5a98e95a8bf73a836e4d9
riumr <atrium_grate.0j@icloud.com>
2024-02-23 23:28:45 +0900

    style : skill stack 블록에 스타일 추가
    - margin 조절

--- portfolio_react/src/components/Skills.jsx
+++ portfolio_react/src/components/Skills.jsx
@@ -14,8 +14,9 @@ export default function Skills(){
 
 const SkillCategory = () => {
     const skillCategoryGroup = ['Frontend','Backend','Deployment','Cooperation','Communication']
+    const categoryStyle = {margin:'15px 0 0 0'}
     const skillCategory = skillCategoryGroup.map((name,index) => (
-        <div key={index} className="fs-5 mt-1">{name}</div>
+        <div key={index} className="fs-5" style={categoryStyle}>{name}</div>
     ))
     return (
         <div className="d-block">{skillCategory}</div>
@@ -28,21 +29,29 @@ const SkillStacks = () => {
     const deploymentStack = ['Firebase','AWS']
     const cooperationStack = ['Git','Github']
     const communicationStack = ['Figma','Slack','Notion','Discord']
-    const stack = (id,stackList) => (
-        <div id={id} className="mt-2">
+
+    const stack = (id,stackList,stackStyle) => (
+        <div id={id} style={stackStyle}>
             {stackList.map((stack,index)=>(
-                <span key={index} className="p-1 border rounded bg-secondary bg-opacity-10">{stack}</span>
+                <span key={index} className="p-1 ms-1 border rounded bg-secondary bg-opacity-10">{stack}</span>
             ))}
         </div>
     )
-    const frontend = stack("frontend",frontendStack)
-    const backend = stack("backend",backendStack)
-    const deployment = stack("deployment",deploymentStack)
-    const cooperation = stack("cooperation",cooperationStack)
-    const communication = stack("communication",communicationStack)
+    
+    const frontendStacStyle = {margin:'16px 0 0 0'}
+    const backendStacStyle = {margin : '17px 0 0 0'}
+    const deploymentStacStyle = {margin : '20px 0 0 0'}
+    const cooperationStacStyle = {margin : '20px 0 0 0'}
+    const communicaitonStacStyle = {margin : '20px 0 0 0'}
+
+    const frontend = stack("frontend",frontendStack,frontendStacStyle)
+    const backend = stack("backend",backendStack,backendStacStyle)
+    const deployment = stack("deployment",deploymentStack,deploymentStacStyle)
+    const cooperation = stack("cooperation",cooperationStack, cooperationStacStyle)
+    const communication = stack("communication",communicationStack,communicaitonStacStyle)
 
     return (
-        <div className="ms-5 mt-1">
+        <div className="ms-5">
             {frontend}
             {backend}
             {deployment}
