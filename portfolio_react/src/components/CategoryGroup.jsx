d0aaa36010e9c3125285ce9d5694a8a98a5026b6
riumr <atrium_grate.0j@icloud.com>
2024-02-24 23:01:58 +0900

    style : CategoryGroup 레이아웃 시스템 grid->flex 변경

--- portfolio_react/src/components/CategoryGroup.jsx
+++ portfolio_react/src/components/CategoryGroup.jsx
@@ -14,13 +14,13 @@ export default function CategoryGroup() {
 const Category = () =>{
     const categoryList=["skills","projects","contact","github"]
     const categoryBlock = categoryList.map((name,index)=>
-        <div key={index} className="col-sm-3 border border-dark rounded py-2 fw-semibold">
+        <div key={index} className="ms-3 border border-dark rounded px-5 py-2 fw-semibold">
             <a href={`#${name}`} className="text-decoration-none text-black">{name.toUpperCase()}</a>
         </div>
     )
     return (
-        <>
+        <div className="d-flex justify-content-center flex-wrap">
             {categoryBlock}
-        </>
+        </div>
     )
 }
