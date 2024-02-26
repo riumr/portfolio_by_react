1e9893036da2da75ec88bd716d19f18ec49126b1
riumr <atrium_grate.0j@icloud.com>
2024-02-26 20:56:33 +0900

    style : margin 추가

--- portfolio_react/src/components/ContentBox.jsx
+++ portfolio_react/src/components/ContentBox.jsx
@@ -7,14 +7,14 @@ export default function ContentBox (props) {
         <div key={index}>{content}</div>
     ))
     const stack = stackContent.map((name,index)=>(
-        <span key={index} className="rounded p-1 bg-secondary bg-opacity-10">{name}</span>
+        <span key={index} className="rounded p-1 ms-1 bg-secondary bg-opacity-10">{name}</span>
     ))
     return (
         <div id="textBox" className="ps-3">
             <p className="fs-3 mb-0">{title}</p>
             <a className="text-decoration-none text-dark" href={link}>source</a>
             <div>{description}</div>
-            <div>{stack}</div>
+            <div className="mt-1">{stack}</div>
         </div>
     )
 }
