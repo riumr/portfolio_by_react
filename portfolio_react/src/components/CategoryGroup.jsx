d63a8af8247543092e68ca65222c6e3d8f2a4e26
riumr <atrium_grate.0j@icloud.com>
2024-02-26 20:54:51 +0900

    style : CategoryGroup 레이아웃 grid system으로 변경

--- portfolio_react/src/components/CategoryGroup.jsx
+++ portfolio_react/src/components/CategoryGroup.jsx
@@ -1,10 +1,10 @@
 export default function CategoryGroup() {
-    const categoryWidth = {
+    const categoryGroupWidth = {
         width:'60rem',
     }
     return (
         <div id="buttonToTitle" className="d-flex justify-content-center">
-            <div className="row text-center mx-3 fs-3 gx-3" style={categoryWidth}>
+            <div className="row text-center mx-3 fs-3 gx-3" style={categoryGroupWidth}>
                 <Category/>
             </div>
         </div>
@@ -13,8 +13,11 @@ export default function CategoryGroup() {
 
 const Category = () =>{
     const categoryList=["skills","projects","contact","github"]
+    const categoryStyle = {
+        width:'145px',
+    }
     const categoryBlock = categoryList.map((name,index)=>
-        <div key={index} className="ms-3 border border-dark rounded px-5 py-2 fw-semibold">
+        <div key={index} className="ms-3 my-1 py-1 border border-dark rounded fw-semibold" style={categoryStyle}>
             <a href={`#${name}`} className="text-decoration-none text-black">{name.toUpperCase()}</a>
         </div>
     )
