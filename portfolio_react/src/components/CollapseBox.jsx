31b3981df7b6addcba92a8d5162cc61fcad96441
riumr <atrium_grate.0j@icloud.com>
2024-02-29 19:21:47 +0900

    feat : collpase 주석 처리. 일반 버튼으로 collpase 버튼 교체

--- portfolio_react/src/components/CollapseBox.jsx
+++ portfolio_react/src/components/CollapseBox.jsx
@@ -9,15 +9,18 @@ export default function CollapseBox (props) {
     }
     return(
         <>
-            <div className="btn btn-outline-secondary d-flex justify-content-center align-items-center w-100 mt-3"
-                data-bs-toggle="collapse" data-bs-target={`#${boxId}`} aria-expanded="false" aria-controls={boxId} style={collapseStyle}>
-                    상세내용 보기
-            </div>
-            <div id={boxId} className="collapse" style={boxStyle}>
-                <div className="card card-body mt-3">
-                    <img src={`image/spaCollapseImg.png`} alt={`${boxId}img`}/>
-                </div>
-            </div>
+            <div className="btn btn-outline-secondary w-100 py-3">상세내용 보기</div>
         </>
+        // <>
+        //     <div className="btn btn-outline-secondary d-flex justify-content-center align-items-center w-100 mt-3"
+        //         data-bs-toggle="collapse" data-bs-target={`#${boxId}`} aria-expanded="false" aria-controls={boxId} style={collapseStyle}>
+        //             상세내용 보기
+        //     </div>
+        //     <div id={boxId} className="collapse" style={boxStyle}>
+        //         <div className="card card-body mt-3">
+        //             <img src='./image/github-logo.png' alt={`${boxId}img`}/>
+        //         </div>
+        //     </div>
+        // </>
     )
 }
