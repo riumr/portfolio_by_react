f88fc44f0f392750c9e1296e1fda7c99b24eecbd
riumr <atrium_grate.0j@icloud.com>
2024-02-29 19:20:46 +0900

    feat : carousel 기능 주석 처리. 이미지 박스로 교체

--- portfolio_react/src/components/Carousel.jsx
+++ portfolio_react/src/components/Carousel.jsx
@@ -6,33 +6,37 @@ export default function Carousel (props) {
         height:'230px',
     }
  
-    const carouselButtonStyle = { 
+    const carouselButtonStyle = {
         color:'black',
     }
 
     const carouselHandler = (prevOrNext,target,imgSrc) => (
         <button className={`carousel-control-${prevOrNext}`} type="button" data-bs-target={target} data-bs-slide={`${prevOrNext}`}>
             <span className={`carousel-control-${prevOrNext}-icon`} aria-hidden="true" style={carouselButtonStyle}>
-                <img src={imgSrc} alt="..."/>
+                <img src={imgSrc} alt=""/>
             </span>
             <span className="visually-hidden">{prevOrNext}</span>
         </button>
     )
     
+    const imgSize = {width:'100%'}
     const carouselImg = (sourceList) => (
-        <div className="carousel-inner">
-            <div className="carousel-item">
-                {Object.values(sourceList).map((source,index)=>(
-                    <img key={index} src={source} className="d-block w-100" alt="..."/>
-                ))}
-            </div>
+        <div>
+            <img src="./image/reactSpaProject.png" alt="github-logo" style={imgSize}/>
         </div>
+        // <div className="carousel-inner">
+        //     <div className="carousel-item d-flex">
+        //         {Object.values(sourceList).map((source,index)=>(
+        //             <img key={index} src={source} className="d-block" alt="..."/>
+        //         ))}
+        //     </div>
+        // </div>
     )
     return (
         <div id={carouselId} className="carousel slide border" style={carouselBoxSize}>
             {carouselImg(imgSource)}
-            {carouselHandler("prev","chevron-compact-left.svg")}
-            {carouselHandler("next",'chevron-compact-right.svg')}
+            {/* {carouselHandler("prev","chevron-compact-left.svg")}
+            {carouselHandler("next",'chevron-compact-right.svg')} */}
         </div>
     )
 }
