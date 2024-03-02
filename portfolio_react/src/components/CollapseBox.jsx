8a45c9e0311ce87eb2fd162cf0cdd79314d05868
riumr <atrium_grate.0j@icloud.com>
2024-03-02 16:44:56 +0900

    feat : useState hook 사용 collapse 기능 구현

--- portfolio_react/src/components/CollapseBox.jsx
+++ portfolio_react/src/components/CollapseBox.jsx
@@ -1,26 +1,43 @@
+import { useState } from "react";
 export default function CollapseBox (props) {
+    const [isCollpased,setCollpased] = useState(true)
+
     const boxId = props.boxId
     const boxImg = props.boxImg
-    const collapseStyle = {
-        height:'50px',
+
+    const collpaseTrigger = () => {
+        setCollpased(!isCollpased)
     }
-    const boxStyle = {
-        transition: 'height 0.5s ease',
+
+    const collpased = {
+        height:'0s',
+        backgroundColor:'gray',
+        transition:'height 0.3s ease',
     }
+    const notCollpased = {
+        height:'max-content',
+        overflow:'hidden',
+        transition:'height 0.3s ease'
+    }
+    const imgStyle = {
+        width:'100%',
+        height:'100%',
+    }
+
     return(
         <>
-            <div id="collpase-button" className="btn btn-outline-dark w-100 py-3">상세내용 보기</div>
+            <div onClick={collpaseTrigger} id="collpase-button" className="btn btn-outline-dark w-100 py-3">상세내용 보기</div>
+            <div id={boxId}>
+                {
+                    isCollpased===true? (
+                        <div style={collpased}></div>
+                    ):(
+                        <div style={notCollpased}>
+                            <img src={boxImg} alt={boxId} style={imgStyle} />
+                        </div>
+                    )
+                }
+            </div>
         </>
-        // <>
-        //     <div className="btn btn-outline-secondary d-flex justify-content-center align-items-center w-100 mt-3"
-        //         data-bs-toggle="collapse" data-bs-target={`#${boxId}`} aria-expanded="false" aria-controls={boxId} style={collapseStyle}>
-        //             상세내용 보기
-        //     </div>
-        //     <div id={boxId} className="collapse" style={boxStyle}>
-        //         <div className="card card-body mt-3">
-        //             <img src='./image/github-logo.png' alt={`${boxId}img`}/>
-        //         </div>
-        //     </div>
-        // </>
     )
 }
