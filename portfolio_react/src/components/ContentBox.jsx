425fce357e7f7c51d8e67482c5c9fd068b029efa
Kimtaehyung <108637691+riumr@users.noreply.github.com>
2024-03-10 17:46:46 +0900

    style : contentBox 내에 프로젝트 배포 사이트로 연결하는 링크 요소 추가
    - source 요소 스타일 복사
    - 사이트 주소가 입력되지 않으면 display:none

--- portfolio_react/src/components/ContentBox.jsx
+++ portfolio_react/src/components/ContentBox.jsx
@@ -1,6 +1,7 @@
 export default function ContentBox (props) {
     const title = props.title
     const link = props.link
+    const site = props.site
     const contentAbout = props.contentAbout
     const stackContent = props.stackContent
     const description = Object.values(contentAbout).map((content,index)=>(
@@ -9,11 +10,28 @@ export default function ContentBox (props) {
     const stack = stackContent.map((name,index)=>(
         <span key={index} className="rounded p-1 ms-1 bg-secondary bg-opacity-10">{name}</span>
     ))
+    let sourceStyle = {
+        textDecoration: 'none',
+        fontWeight: 'bold',
+        border: '1px solid #000',
+        borderRadius: '0.25rem',
+        padding: '0.25rem',
+    }
+    let siteStyle = {
+        ...sourceStyle,
+    };
+
+    if (site==""){
+        siteStyle={
+            display:'none',
+        }
+    }
     return (
         <div id="text-box" className="ps-3 mt-2">
             <p className="fs-3 mb-0">{title}</p>
             <div id="ref-link" class="my-1">
-                <a class="text-decoration-none fw-bold border rounded p-1" href={link}>source</a>
+                <a id="source-link" href={link} style={sourceStyle}>source</a>
+                <a id="site-link" href={site} style={siteStyle} className="ms-1">site</a>
             </div>
             <div>{description}</div>
             <div className="mt-1">{stack}</div>
