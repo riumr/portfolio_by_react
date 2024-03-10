1a3199fc0598e1c00caddfba06f0d3ea37658338
Kimtaehyung <108637691+riumr@users.noreply.github.com>
2024-03-10 17:48:46 +0900

    content : 프로젝트 배포 사이트 props 추가 및 기타
    - Text 분석 프로젝트 주석처리
    - spa 게시판 collpase 주석 처리 해제

--- portfolio_react/src/components/Projects.jsx
+++ portfolio_react/src/components/Projects.jsx
@@ -16,8 +16,9 @@ export default function Projects() {
                         <Project 
                             projectName="Personal Project" carouselId="spaProject" carouselImg='./image/reactSpaProject.png'
                             contentTitle="SPA 게시판" contentAbout={spaPjtContent} stackContent={spaPjtStack} contentLink="https://github.com/riumr/browse_supabase"
+                            siteLink="https://supabasehost.web.app"
                         />
-                        {/* <CollapseBox boxId="spaPjt" boxImg='./image/spa-collapse-img.png'/> */}
+                        <CollapseBox boxId="spaPjt" boxImg='./image/spa-collapse-img.png'/>
 
                     </div>
                     {/* Team Project */}
@@ -25,6 +26,7 @@ export default function Projects() {
                         <Project
                             projectName="Team Project" carouselId="teamProject" carouselImg='./image/javachip-item.jpg'
                             contentTitle="원두판매사이트" contentAbout={djangoPjtContent} stackContent={djangoPjtStack} contentLink="https://github.com/riumr/Java-chip"
+                            siteLink=""
                         />
                         {/* <CollapseBox boxId="teamPjt" boxImg="./image/javachip-item.jpg"/> */}
                     </div>
@@ -33,15 +35,17 @@ export default function Projects() {
                         <Project
                             projectName="Profile Page" carouselId="portfolio" carouselImg='./image/p_1.png'
                             contentTitle="포트폴리오" contentAbout={profilePjtContent} stackContent={profilePjtStack} contentLink="https://github.com/riumr/riumr.github.io"
+                            siteLink=""
                         />
                     </div>
                     {/* Text 분석 프로젝트 */}
-                    <div className="pt-3">
+                    {/* <div className="pt-3">
                         <Project
                             projectName="Text Analysis Project" carouselId="textAnalysis" carouselImg='./image/textProject.png'
                             contentTitle="민원 데이터 시각화" contentAbout={textPjtContent} stackContent={textPjtStack} contentLink="https://github.com/analysistTH/complaintsAnaysis"
+                            siteLink=""
                         />
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
@@ -55,12 +59,13 @@ const Project = (props) => {
     const contentAbout = props.contentAbout
     const stackContent = props.stackContent
     const contentLink = props.contentLink
+    const siteLink = props.siteLink
     return (
         <div className="pt-3">
             <h3 className="semibold">{props.projectName}</h3>
             <div className="d-flex flex-wrap py-2 ps-2">
                 <Carousel carouselId={carouselId} carouselImg={carouselImg}/>
-                <ContentBox title={contentTitle} contentAbout={contentAbout} stackContent={stackContent} link={contentLink}/>
+                <ContentBox title={contentTitle} contentAbout={contentAbout} stackContent={stackContent} link={contentLink} site={siteLink}/>
             </div>
         </div>
     )
