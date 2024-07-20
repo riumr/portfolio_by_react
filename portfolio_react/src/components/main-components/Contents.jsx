0ff7446eea97994a9d6ae1f9d022c5facb4e773e
serviceapperance <108637691+riumr@users.noreply.github.com>
2024-07-20 21:54:20 +0900

    [Content] 할인판매 서비스 내용, component 추가

--- portfolio_react/src/components/main-components/Contents.jsx
+++ portfolio_react/src/components/main-components/Contents.jsx
@@ -1,4 +1,4 @@
-export const spaPjtContent = [
+const spaPjtContent = [
     '소요기간 : 4주 | 인원 : 1명',
     '역할 : 프론트엔드 기능 및 레이아웃 개발',
     '∙ supabase를 사용해 DB crud 구현',
@@ -6,7 +6,7 @@ export const spaPjtContent = [
     '∙ firebase hosting을 사용해 배포',
 ]
 
-export const djangoPjtContent = [
+const djangoPjtContent = [
     '소요기간 : 3주 | 인원 : 5명',
     '역할 : 팀장. 발표자. 상품목록∙상품상세 페이지 프론트엔드∙백엔드 개발',
     '∙ 상품 목록 페이지 및 상품 상세 페이지 프론트엔드 구현',
@@ -14,23 +14,39 @@ export const djangoPjtContent = [
     '∙ 참고 페이지에서 상품 정보 및 이미지 크롤링, DB에 저장',
 ]
 
-export const profilePjtContent = [
+const profilePjtContent = [
     '인원 : 1명',
     '역할 : 프론트엔드 템플릿 구현 및 내용 구성',
     '∙ Javascript를 사용하지 않은 프로젝트',
-    '∙ 유지보수성을 향상시키기 위해 React로 refactoring'
+    '∙ 유지보수성을 향상시키기 위해 React로 refactoring',
+    '∙ 유지보수성을 향상시키기 위해 React로 refactoring',
 ]
 
-export const textPjtContent = [
+const textPjtContent = [
     '소요기간 : 3개월 | 인원 : 1명',
     '역할 : 기획∙분석∙보고서 작성',
-    '∙ 텍스트 데이터 기간별∙카테고리별 분류'
+    '∙ 텍스트 데이터 기간별∙카테고리별 분류',
+    '∙ 텍스트 데이터 기간별∙카테고리별 분류',
+    '∙ 텍스트 데이터 기간별∙카테고리별 분류',
 ]
 
-export const spaPjtStack = ['React','supabase','Firebase','Figma']
+const salePjtContent = [
+    '소요기간 : 4주 | 인원 : 4명',
+    '역할 : 프론트엔드',
+    '∙ 프로젝트 전체 요약',
+    '∙ 사용한 기술 중 주요한 기술과 성과',
+    '∙ 프로젝트의 의의',
+]
 
-export const djangoPjtStack = ['HTML','CSS','bootstrap','Django','SQLite','postgresql','Figma','Discord','Notion','AWS EC2']
+const spaPjtStack = ['React','supabase','Firebase','Figma']
 
-export const profilePjtStack = ['React','HTML','CSS','bootstrap','Figma','Github page']
+const djangoPjtStack = ['HTML','CSS','bootstrap','Django','SQLite','postgresql','Figma','Discord','Notion','AWS EC2']
 
-export const textPjtStack = ['python','Google colab','Tableu']
+const profilePjtStack = ['React','HTML','CSS','bootstrap','Figma','Github page']
+
+const textPjtStack = ['python','Google colab','Tableu']
+
+const salePjtStack = ['React','Figma','Spring','AWS',"MySql",'Jira','Slack']
+
+export {spaPjtContent, djangoPjtContent, profilePjtContent, textPjtContent, salePjtContent}
+export {spaPjtStack, djangoPjtStack, profilePjtStack, textPjtStack, salePjtStack}
--- portfolio_react/src/components/main-components/Projects.jsx
+++ portfolio_react/src/components/main-components/Projects.jsx
@@ -1,8 +1,8 @@
 import Carousel from "../main-components/Carousel"
 import ContentBox from "../main-components/ContentBox"
 import CollapseBox from "../main-components/CollapseBox"
-import { spaPjtContent,djangoPjtContent,profilePjtContent,textPjtContent } from "../main-components/Contents"
-import { spaPjtStack,djangoPjtStack,profilePjtStack,textPjtStack } from "../main-components/Contents"
+import { spaPjtContent,djangoPjtContent,profilePjtContent,textPjtContent, salePjtContent } from "../main-components/Contents"
+import { spaPjtStack,djangoPjtStack,profilePjtStack,textPjtStack, salePjtStack } from "../main-components/Contents"
 
 
 export default function Projects() {
@@ -12,20 +12,19 @@ export default function Projects() {
                 <div className="d-flex flex-column">
                     <h2 className="fs-1 fw-bold mt-5">PROJECTS</h2>
                     {/* Personal Project */}
-                    <div className="pt-3">
+                    {/* <div className="pt-3">
                         <Project 
                             projectName="Personal Project" carouselId="spaProject" carouselImg='./image/reactSpaProject.png'
                             contentTitle="SPA 게시판" contentAbout={spaPjtContent} stackContent={spaPjtStack} contentLink="https://github.com/riumr/browse_supabase"
                             siteLink="https://supabasehost.web.app"
                         />
                         <CollapseBox boxId="spaPjt" boxImg='./image/spa-collapse-img.png'/>
-
-                    </div>
+                    </div> */}
                     {/* Team Project */}
                     <div className="pt-3">
                         {/* 글자가 전체 화면의 너비 늘리는 문제 발생 */}
                         <Project
-                            projectName="Team Project" carouselId="teamProject" carouselImg='./image/javachip-item.jpg'
+                            projectName="Django 팀 프로젝트" carouselId="teamProject" carouselImg='./image/javachip-item.jpg'
                             contentTitle="원두판매사이트" contentAbout={djangoPjtContent} stackContent={djangoPjtStack} contentLink="https://github.com/riumr/Java-chip"
                             siteLink=""
                         />
@@ -36,6 +35,14 @@ export default function Projects() {
                         <Project
                             projectName="Profile Page" carouselId="portfolio" carouselImg='./image/p_1.png'
                             contentTitle="포트폴리오" contentAbout={profilePjtContent} stackContent={profilePjtStack} contentLink="https://github.com/riumr/riumr.github.io"
+                            siteLink=""
+                        />
+                    </div>
+                    {/* new project */}
+                    <div className="pt-3">
+                        <Project
+                            projectName="Team 프로젝트-프론트엔드 담당" carouselId="saleProject" carouselImg='./image/p_1.png'
+                            contentTitle="할인식품예약구매 웹 앱" contentAbout={salePjtContent} stackContent={salePjtStack} contentLink=""
                             siteLink=""
                         />
                     </div>
